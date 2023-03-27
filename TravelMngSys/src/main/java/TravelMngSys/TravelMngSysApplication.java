b24032f1a9c8e0a32231fcc05d4817bffcadabf0
albonidrizi <albonidrizi@gmail.com>
2023-03-27 15:37:22 +0200

    starting project

---
+++ TravelMngSys/src/main/java/TravelMngSys/TravelMngSysApplication.java
@@ -0,0 +1,13 @@
+package TravelMngSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelMngSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelMngSysApplication.class, args);
+	}
+
+}
---
+++ TravelMngSys/src/test/java/TravelMngSysApplicationTests.java
@@ -0,0 +1,13 @@
+package TravelMngSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelMngSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
